988d32efa2fc4718ec192889ad307bab159c1d66
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-24 10:54:13 -0300

    fix: lint fix

--- src/controllers/httpController.js
+++ src/controllers/httpController.js
@@ -1,14 +1,14 @@
-const axios = require(`axios`)
+const axios = require('axios')
 
-module.exports={
-  async getGif(){
+module.exports = {
+  async getGif () {
 
   },
-  async getRecipe(ingredients){
-    var url = `http://www.recipepuppy.com/api/?i=`
-    ingredients.map((ingredient,index) => {
-      index == 0 ?  url+= `${ingredient}`: url+=`,${ingredient}`
-    });
+  async getRecipe (ingredients) {
+    var url = 'http://www.recipepuppy.com/api/?i='
+    ingredients.map((ingredient, index) => {
+      index === 0 ? url += `${ingredient}` : url += `,${ingredient}`
+    })
     const resultado = await axios.get(url)
     return resultado.data
   }
--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,24 +1,26 @@
-const httpController = require(`./httpController`)
+const httpController = require('./httpController')
+const sortUtils = require('../utils/sortUtils')
+const stringUtils = require('../utils/stringUtils')
 
-
-module.exports= {
-  async returnRecipe(req,res){
+module.exports = {
+  async returnRecipe (req, res) {
     const ingredients = req.query.i
-    let keywords = (ingredients.split('i=')[0]).split(',')
+    const keywords = (ingredients.split('i=')[0]).split(',')
 
     const returnOfRecipePuppy = await httpController.getRecipe(keywords)
 
-    let recipes = await returnOfRecipePuppy.results.map( recipe => { 
-      const titleWithoutMarks = recipe.title.replace(/\n/g,'')
-      let arrayOfIngredients = recipe.ingredients.split(',')
-      const ingredientsCleaned = arrayOfIngredients.map(Function.prototype.call.bind(String.prototype.trim)) 
-      //for any reason, the trim() method wasnt working :/
+    const recipes = await returnOfRecipePuppy.results.map(recipe => {
+      const titleWithoutMarks = stringUtils.removeMarks(recipe.title)
+      const arrayOfIngredients = recipe.ingredients.split(',')
+      const ingredientsCleaned = arrayOfIngredients.map(elem => stringUtils.removeBlankSpaces(elem))
+      console.log(ingredientsCleaned)
+      // for any reason, the trim() method wasnt working :/
       const ingredientsSorted = ingredientsCleaned.sort()
-      return { title:titleWithoutMarks , link: recipe.href, ingredients:ingredientsSorted} 
+      return { title: titleWithoutMarks, link: recipe.href, ingredients: ingredientsSorted }
     })
-    const teste ={
+    const teste = {
       keyowrds: keywords,
-      recipes:recipes
+      recipes: recipes
     }
     return res.status(200).send(teste)
   }
