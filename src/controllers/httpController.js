b8473f0a1eadbf20dd9b08ab8daba549e85043da
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-23 23:10:26 -0300

    feat: httpController

--- src/controllers/httpController.js
+++ src/controllers/httpController.js
@@ -6,7 +6,7 @@ module.exports={
   },
   async getRecipe(url1){
     console.log(url1)
-    const url = `http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3`
+    const url = `http://www.recipepuppy.com/api/?i=${onions},${garlic}&q=omelet&p=3`
     const resultado = await axios.get(url)
     console.log(resultado.data)
   }
