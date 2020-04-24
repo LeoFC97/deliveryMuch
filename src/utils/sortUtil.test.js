aafd50756bff279e0ac31f6df1e2138f92d229dc
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-24 11:25:16 -0300

    fix: describe text

--- src/utils/sortUtil.test.js
+++ src/utils/sortUtil.test.js
@@ -1,6 +1,6 @@
 const sortUtil = require('./sortUtils')
 
-describe('alphabeticalSort', () => {
+describe('Sorting util', () => {
   test('Should return ordered array', () => {
     const unOderderedSimpleArray = ['z', 's', 'a', 'b']
     const orderedSimpleArray = sortUtil.alphabeticalSort(unOderderedSimpleArray)
