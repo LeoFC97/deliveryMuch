ada1005f5d300f0c2bbf787cb83b60c295c81543
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-25 02:05:22 -0300

    fix: eslint fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-const express = require(`express`)
-const routes = require(`./routes.js`)
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express')
+const routes = require('./routes.js')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
-const port =3333
+const port = 3333
 
-app.use(`/`,routes)
+app.use('/', routes)
 app.use(cors())
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-
-app.listen(port,()=>{
-  console.log(`app running @ port: `+port)
+app.listen(port, () => {
+  console.log('app running @ port: ' + port)
 })
