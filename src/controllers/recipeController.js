ceaf677fbc8c953e6b4f75d264793656c24faae2
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-25 02:06:38 -0300

    fix: changed variable name for respect clean code principles

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -7,11 +7,11 @@ module.exports = {
   async returnRecipe (req, res) {
     const { i } = req.query
     if (!i) {
-      const errorObject = errorController.missingParams()
-      return res.status(errorObject.status).send(errorObject.errorSring)
+      const error = errorController.missingParams()
+      return res.status(error.status).send(error.errorMensage)
     }
-    const keywords = (i.split('i=')[0]).split(',')
 
+    const keywords = (i.split('i=')[0]).split(',')
     const returnOfRecipePuppy = await httpController.getRecipe(keywords)
 
     const recipes = await returnOfRecipePuppy.results.map(recipe => {
@@ -21,10 +21,21 @@ module.exports = {
       const ingredientsSorted = sortUtils.alphabeticalSort(ingredientsCleaned)
       return { title: titleWithoutMarks, link: recipe.href, ingredients: ingredientsSorted }
     })
-    const teste = {
+
+    for (let i = 0; i < recipes.length; i++) {
+      const returnOfGifRequest = await httpController.getGif(recipes[i].title)
+      recipes[i].gif = returnOfGifRequest.data[0].images.original.url
+      console.log(i)
+    }
+
+    // const returnOfGifRequest = await httpController.getGif(recipe.title)
+    // return returnOfGifRequest.data[0].images.original.url
+    console.log(recipes)
+
+    const objectToReturn = {
       keyowrds: keywords,
       recipes: recipes
     }
-    return res.status(200).send(teste)
+    return res.status(200).send(objectToReturn)
   }
 }
