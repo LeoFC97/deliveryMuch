d2a2c058040ef536aeab94e5c782540a46fafaa7
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-26 10:57:02 -0300

    feat: add port to env file

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const routes = require('./routes.js')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const app = express()
-const port = 3333
+const port = process.env.PORT
 const host = '0.0.0.0'
 
 app.use('/', routes)
 app.use(cors())
 app.use(bodyParser.json())
-
 app.listen(port, host, () => {
   console.log('app running @ port: ' + port)
 })
