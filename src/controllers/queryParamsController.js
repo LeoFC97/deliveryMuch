dc08f5b758cb60fed7145242c49d82ab88bcc817
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-26 12:26:03 -0300

    feat: check array size TDD

--- src/controllers/queryParamsController.js
+++ src/controllers/queryParamsController.js
@@ -1,5 +1,11 @@
 module.exports = {
   splitQueryIntoArray (queryToBeSplited) {
     return (queryToBeSplited.split('i=')[0]).split(',')
+  },
+  checkNumberOfParams (arrayToBeCheked) {
+    if (arrayToBeCheked.length > 0 && arrayToBeCheked.length < 4) {
+      return true
+    }
+    return false
   }
 }
