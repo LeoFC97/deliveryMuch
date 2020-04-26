b4e5ecb63e6af1689e98cafa77b9df1e117c25aa
LeoFC97 <leonardo.fc2@outlook.com>
2020-04-26 16:34:45 -0300

    Fix: text mensage on test files

--- src/controllers/queryParams.test.js
+++ src/controllers/queryParams.test.js
@@ -1,20 +1,20 @@
 const queryParamsController = require('./queryParamsController')
 
 describe('query ParamsController ', () => {
-  test('Should split the query and return an array', () => {
+  test('Splits the query and returns an array', () => {
     const originalQuery = 'tomato,onions,avocado'
     const array = queryParamsController.splitQueryIntoArray(originalQuery)
     expect(array).toEqual(['tomato', 'onions', 'avocado'])
   })
-  test('Should return true if the array size > 1 ', () => {
+  test('Returns true if the array size > 1 ', () => {
     const arrayMock = ['tomato', 'onions', 'avocado']
     expect(queryParamsController.checkNumberOfParams(arrayMock)).toBe(true)
   })
-  test('Should return false if the array size = 0 ', () => {
+  test('Returns false if the array size = 0 ', () => {
     const arrayMock = []
     expect(queryParamsController.checkNumberOfParams(arrayMock)).toBe(false)
   })
-  test('Should return false if the array size > 3 ', () => {
+  test('Returns false if the array size > 3 ', () => {
     const arrayMock = ['tomato', 'onions', 'avocado', 'watermelon']
     expect(queryParamsController.checkNumberOfParams(arrayMock)).toBe(false)
   })
